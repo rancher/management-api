fa7dbc376225386ed5873ca861ad162a0f4a3399
Darren Shepherd <darren@rancher.com>
2018-01-22 17:07:40 -0700

    Fix compilation error

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"context"
-	"os"
-
-	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/rancher/management-api/server"
 	"github.com/rancher/types/config"
@@ -29,11 +27,16 @@ func run() error {
 		return err
 	}
 
-	handler, err := server.New(context.Background(), management)
+	ctx := context.Background()
+
+	var handler http.Handler
+	handler, err = server.New(ctx, 8080, 8443, management, func() http.Handler {
+		return handler
+	})
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("Listening on 0.0.0.0:1234")
-	return http.ListenAndServe("0.0.0.0:1234", handler)
+	<-ctx.Done()
+	return ctx.Err()
 }
